05c4132ee7bccfed03d82448e183b86b51665442
pdong2001 <79776591+pdong2001@users.noreply.github.com>
2021-07-16 07:32:05 +0700

    commit

--- resources/js/base/slider.js
+++ resources/js/base/slider.js
@@ -43,31 +43,5 @@ $(document).ready(function() {
             }
         ]
     });
-    $(".content-menu.slider").slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 3,
-        arrows: false,
-        autoplay: true,
-        pauseOnFocus: true,
-        autoplaySpeed: 5000,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-            },
-            breakpoint: 800,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            },
-            breakpoint: 400,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        }]
-    });
+
 });
--- resources/js/home/content-menu.js
+++ resources/js/home/content-menu.js
@@ -1,6 +1,41 @@
 $(document).ready(function() {
+    const contentMenuItemTemplate = $('.content-menu-item').clone();
+    const contentMenu = $('.content-menu.slider');
+    $('.row-ID').each(function(index) {
+        var temp = contentMenuItemTemplate.clone();
+        temp.find('a').attr('href', '#' + $(this).attr('id'))
+        temp.removeClass('content-menu-item-template');
+        temp.find('a').html($(this).find('.title:first-of-type h2').html());
+        temp.appendTo(contentMenu);
+        console.log(temp);
+    });
     $('.content-menu-item').click(function() {
         $('.content-menu-item.active').removeClass("active");
         $(this).addClass('active');
-    })
+    });
+    $(".content-menu.slider").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        arrows: false,
+        autoplay: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            },
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            },
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        }]
+    });
 });
